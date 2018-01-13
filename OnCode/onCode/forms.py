f5250498ba05b216138a8b777e40dfcbcfb4c3ca
Unknown <eslstelutza@gmail.com>
2018-01-13 14:57:43 +0200

    Final Update

---
+++ OnCode/onCode/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from  models import *
+
+class UserCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'username', 'first_name', 'last_name', 'password'
+        )
+
+    password = forms.CharField(widget=forms.PasswordInput())
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+
+class AddCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = (
+            'birth_date', 'university')
+    birth_date = forms.DateField(required = True)
+    university = forms.CharField(required = True)
--- OnCode/onCode/views.py
+++ OnCode/onCode/views.py
@@ -4,18 +4,19 @@ from models import *
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from onCode.forms import UserEditForm
-from django.urls import reverse
 from forms import *
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+
 
 # Create your views here.
 class LeaderboardView(ListView):
-    #import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
     model = UserProfile
     template_name = 'leaderboard-view.html'
     context_object_name = 'users'
     ordering = ['-score']
-    
+
 
 class ProblemDetailView(DetailView):
     template_name = 'problem.html'
@@ -27,46 +28,90 @@ class ViewComments(ListView):
     template_name = 'viewComment.html'
     model = Comment
     context_object_name = 'Comment'
+
     def get_queryset(self, *args, **kwargs):
         return Comment.objects.filter(problem_id=self.kwargs.get('pk'))
 
-class AddComment(CreateView):
-    template_name = 'addComment.html'
-    model = Comment
-    context_object_name = 'Comment'
 
 class UserProfileDetailView(DetailView):
     template_name = 'user-profile-details.html'
     model = UserProfile
     context_object_name = 'userprofile'
 
+
 class ProblemsListView(ListView):
     model = Problem
-    template_name = 'problems-view-list.html'
-    context_object_name = 'problems'   
-    
-    
+    template_name = 'problem-list-view.html'
+    context_object_name = 'problems'
+
+
 class UserCreateView(CreateView):
     form_class = UserCreateForm
     template_name = 'user-create-view.html'
     model = User
 
     def get_success_url(self, *args, **kwargs):
-        profile = UserProfile(user_pk = self.object, score=0)
+        profile = UserProfile(user_pk=self.object, score=0)
         profile.save()
         return reverse(
-            'edit_user_profile_view', 
+            'edit_user_profile_view',
             kwargs={
                 'pk': self.object.pk
             }
         )
 
+
+class AddComment(CreateView):
+    template_name = 'addComment.html'
+    model = Comment
+    form_class = AddCommentForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user_id = User.objects.get(pk=self.request.user.id)
+        self.object.problem_id = Problem.objects.get(pk=self.kwargs.get('pk'))
+        self.object.save()
+        return super(AddComment, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse(
+            'view_comment',
+            kwargs={
+                'pk': self.kwargs.get('pk')
+            }
+        )
+
+
+def login_view(request):
+    context = {}
+    if request.method == 'GET':
+        form = LoginForm()
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request=request,
+                      user=user)
+                return redirect('problem_list_view')
+            else:
+                context['error_message'] = 'Wrong username or password!'
+    context['form'] = form
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    if request.method == 'GET':
+        logout(request)
+        return redirect('login')
+
+
 class UserUpdateView(UpdateView):
     template_name = 'user-edit-profile-view.html'
     form_class = UserEditForm
     model = UserProfile
-    
-    
+
     def get_success_url(self, *args, **kwargs):
         return reverse(
             'user-profile-view',
@@ -74,5 +119,3 @@ class UserUpdateView(UpdateView):
                 'pk': self.kwargs['pk']
             }
         )
-        
-        
