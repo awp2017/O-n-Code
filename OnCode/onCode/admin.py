84c0be6f5b6bf63a50805526a959f2f7d17ea98f
HorridBro <mihneasaracin@yahoo.com>
2018-01-13 10:34:33 +0200

    admin register all models

--- OnCode/onCode/admin.py
+++ OnCode/onCode/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from models import *
 
+admin.site.register(UserProfile)
+admin.site.register(Problem)
+admin.site.register(Rezolvari)
+admin.site.register(Comment)
 # Register your models here.
