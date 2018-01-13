03e363a32117a356d94c724a068a772a80ecb8b3
HorridBro <mihneasaracin@yahoo.com>
2018-01-13 10:55:02 +0200

    added layout.html

--- OnCode/onCode/urls.py
+++ OnCode/onCode/urls.py
@@ -2,5 +2,7 @@
 from django.conf.urls import url
 
 urlpatterns = [
-
+    url(r'^problem/(?P<pk>[0-9]+)/$',
+        views.TaskDetailView.as_view(),
+        name='task_detail'),
 ]
--- OnCode/onCode/views.py
+++ OnCode/onCode/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.views.generic import View, Temp
+from django.views.generic import DetailView
 from django.shortcuts import render
+from models import Problem
 
 # Create your views here.
+
+class ProblemView(DetailView):
+    model = Problem
