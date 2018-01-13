8fdc5a64478094f54de1753ce170ca949cd5001c
Bogdan Alexandrescu <bogdanalexandrescu96@gmail.com>
2018-01-13 08:00:39 +0000

    added problem's model

--- OnCode/onCode/models.py
+++ OnCode/onCode/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+difficulties = [('1','Low'), ('2','Medium'), ('3', 'High'),  ('4', 'God')]
 
 # Create your models here.
+class Problem(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=350)
+    difficulty = models.CharField(max_length=50, choices=difficulties)
+    hints = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
