58b99cd6d91c691f94b4b03307aeaa401807bb1c
Bogdan Alexandrescu <bogdanalexandrescu96@gmail.com>
2018-01-13 10:43:28 +0000

    changed urls

--- OnCode/onCode/urls.py
+++ OnCode/onCode/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from onCode.views import LeaderboardView, UserProfileDetailView
+from onCode.views import (LeaderboardView, UserProfileDetailView, 
+                        AddComment, ViewComments, ProblemDetailView)
 
 urlpatterns = [
-    #url(r'^problem/(?P<pk>[0-9]+)/$',views.TaskDetailView.as_view(),name='task_detail'),
     url(r'^leaderboard/$', LeaderboardView.as_view(), name='leaderboard_view'),
     url(r'^userprofile/(?P<pk>[0-9]+)/$', UserProfileDetailView.as_view(), name='user-profile-view'),
+    url(r'^Problem/(?P<pk>[0-9]+)/$',ProblemDetailView.as_view(),name='problem_detail'),
+    url(r'^Problem/(?P<pk>[0-9]+)/ViewComments/$',ViewComments.as_view(),name="view_comment"),
+    url(r'^Problem/(?P<pk>[0-9]+)/AddComment/$',AddComment.as_view(),name="add_comment")
 ]
