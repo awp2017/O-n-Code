a6846a646c01323694ad77202dbb66f986dfa5b7
Bogdan Alexandrescu <bogdanalexandrescu96@gmail.com>
2018-01-13 09:43:26 +0000

    added leaderboard view

--- OnCode/onCode/models.py
+++ OnCode/onCode/models.py
@@ -8,12 +8,14 @@ difficulties = [('1','Low'), ('2','Medium'), ('3', 'High'),  ('4', 'God')]
 # Create your models here.
 
 class UserProfile(models.Model):
-    user_pk = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_pk = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
     birth_date = models.DateField(blank=True)
     university = models.CharField(max_length=100)
     score = models.IntegerField()
 
-
+    def __str__(self):
+        return self.username
+    
 class Problem(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=350)
@@ -29,7 +31,7 @@ class Rezolvari(models.Model):
     answer = models.CharField(max_length=150000)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
 
-
+    
 
 class Comment(models.Model):
     problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
--- OnCode/onCode/urls.py
+++ OnCode/onCode/urls.py
@@ -1,8 +1,7 @@
-
 from django.conf.urls import url
+from onCode.views import LeaderboardView
 
 urlpatterns = [
-    url(r'^problem/(?P<pk>[0-9]+)/$',
-        views.TaskDetailView.as_view(),
-        name='task_detail'),
+    #url(r'^problem/(?P<pk>[0-9]+)/$',views.TaskDetailView.as_view(),name='task_detail'),
+    url(r'^leaderboard/$', LeaderboardView.as_view(), name='leaderboard_view'),
 ]
