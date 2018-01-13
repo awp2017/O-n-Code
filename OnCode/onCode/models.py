ea1862e1721e8f17b82ee715a265e61b524d3f37
Teodor Sposib <teodor.sposib@mindit.ro>
2018-01-13 10:04:51 +0200

    Create Comment Model

--- OnCode/onCode/models.py
+++ OnCode/onCode/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    problem_id = models.ForeignKey(Problem, blank=True)
+    user_id = models.ForeignKey(User, blank=True)
+    text = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.text
